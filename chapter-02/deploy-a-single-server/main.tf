b09899a0dc31ff48eede0bd9862d643d7ae9b45f
tinhcao <caodangtinh@gmail.com>
2023-12-28 20:28:05 +0700

    2.2.deploy-cluster-server

--- chapter-02/deploy-a-single-server/main.tf
+++ chapter-02/deploy-a-single-server/main.tf
@@ -2,18 +2,17 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami                         = "ami-0fa377108253bf620"
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.instance.id]
-  user_data                   = <<-EOF
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fa377108253bf620"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data       = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  user_data_replace_on_change = true
-  tags = {
-    "Name" = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -27,15 +26,38 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default_subnets.ids
+  min_size             = 2
+  max_size             = 10
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_subnets" "default_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default_vpc.id]
+  }
+}
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
-}
+# output "public_ip" {
+#   value       = aws_instance.example.public_ip
+#   description = "The public IP address of the web server"
+# }
 
 # variable "number_example" {
 #   description = "An example of a number variable in Terraform"
