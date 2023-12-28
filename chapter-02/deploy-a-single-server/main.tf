fad9dc57a765e9792296383c61183bf813606694
Tinh Dang Cao <tinhdang.cao@unite.eu>
2023-12-28 22:04:29 +0700

    2.2.4.deploy-cluster-web-with-lb

--- chapter-02/deploy-a-single-server/main.tf
+++ chapter-02/deploy-a-single-server/main.tf
@@ -29,12 +29,80 @@ resource "aws_security_group" "instance" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default_subnets.ids
+  target_group_arns    = [aws_lb_target_group.asg_target_group.arn]
+  health_check_type    = "ELB"
   min_size             = 2
   max_size             = 10
   tag {
     key                 = "Name"
     value               = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default_subnets.ids
+  security_groups    = [aws_security_group.alb_security_group.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Page not found"
+      status_code  = "400"
+    }
+  }
+}
+
+resource "aws_security_group" "alb_security_group" {
+  name = "terraform-example-alb"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_lb_target_group" "asg_target_group" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default_vpc.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "lb_listener_rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg_target_group.arn
   }
 }
 
@@ -52,6 +120,11 @@ data "aws_vpc" "default_vpc" {
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The DNS name of the load balancer"
 }
 
 # output "public_ip" {
